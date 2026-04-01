dcb9a3e26f720f0e722c07cb9e3bd145c9bda27d
AlexLexicon <Alexrs.business@outlook.com>
2026-03-31 17:16:19 -0700

    removed more abigous conversions

--- Lexicom.ConsoleApp.Amenities/WriteLineSegment.cs
+++ Lexicom.ConsoleApp.Amenities/WriteLineSegment.cs
@@ -6,7 +6,6 @@ public readonly struct WriteLineSegment
     public static implicit operator WriteLineSegment(char value) => new(value.ToString(), null);
     public static implicit operator WriteLineSegment(char[]? buffer) => new(buffer?.ToString(), null);
     public static implicit operator WriteLineSegment(double value) => new(value.ToString(), null);
-    public static implicit operator WriteLineSegment(long value) => new(value.ToString(), null);
     public static implicit operator WriteLineSegment(string? value) => new(value?.ToString(), null);
     public static implicit operator WriteLineSegment(ulong value) => new(value.ToString(), null);
 
@@ -14,7 +13,6 @@ public readonly struct WriteLineSegment
     public static implicit operator WriteLineSegment((char value, ConsoleColor color) segment) => new(segment.value.ToString(), segment.color);
     public static implicit operator WriteLineSegment((char[]? buffer, ConsoleColor color) segment) => new(segment.buffer?.ToString(), segment.color);
     public static implicit operator WriteLineSegment((double value, ConsoleColor color) segment) => new(segment.value.ToString(), segment.color);
-    public static implicit operator WriteLineSegment((long value, ConsoleColor color) segment) => new(segment.value.ToString(), segment.color);
     public static implicit operator WriteLineSegment((object? value, ConsoleColor color) segment) => new(segment.value?.ToString(), segment.color);
     public static implicit operator WriteLineSegment((string? value, ConsoleColor color) segment) => new(segment.value?.ToString(), segment.color);
     public static implicit operator WriteLineSegment((ulong value, ConsoleColor color) segment) => new(segment.value.ToString(), segment.color);
