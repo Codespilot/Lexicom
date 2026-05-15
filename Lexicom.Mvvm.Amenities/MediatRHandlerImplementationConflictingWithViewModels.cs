d8d9240e072a463be60fc0d4271d410ac4a80dbb
AlexLexicon <Alexrs.business@outlook.com>
2026-05-14 17:34:38 -0700

    removed mvvm amenities since its no longer needed

--- Lexicom.Mvvm.Amenities/MediatRHandlerImplementationConflictingWithViewModels.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-
-namespace Lexicom.Mvvm.Amenities;
-public class MediatRHandlerImplementationConflictingWithViewModels<THandler>(THandler implementation, ServiceLifetime serviceLifetime)
-{
-    public ServiceLifetime ServiceLifetime { get; } = serviceLifetime;
-    public THandler Implementation { get; } = implementation;
-}
